8205ff86a81a81bf32d6370511ef72b0fa974252
Jam19th <jam19th@gmail.com>
2023-08-03 21:37:33 -0700

    Finish Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  return (
+const [gitHubName, setGitHubName] = useState('')
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/Jam19th')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+        setGitHubName(data.name)
+    })
+}, [])
+
+return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubName}</h2>
     </div>
-  );
+);
 }
 
-export default App;
+export default App
+    
